5bf42367501aaf64bf94ec7679c837557fd62051
Victor Petrichenko <korrector666@gmail.com>
2021-04-10 18:47:35 +0300

    turtle

---
+++ import graphics as gr.py
@@ -0,0 +1,24 @@
+# import graphics as gr
+
+# def build_house(window,upper_left_conner, windth):
+#     """ Функция которая рисует дом """
+#     pass
+
+# window = gr.GpaphWin("Russia game", 300, 300)
+# build_house(window, gr.Point(100,100),100)
+
+# print("Ура, дос построен")
+
+import turtle
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+    
